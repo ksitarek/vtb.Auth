e8d44928a7ef47b0a75db068e4180c2f3091c122
Krystian Sitarek <krystiansitarek@gmail.com>
2021-01-31 14:55:29 +0100

    Added messaging filters for tenant header

---
+++ vtb.Auth.Tenant/MessagingFilters/ConsumerTenantFilter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using GreenPipes;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace vtb.Auth.Tenant.MessagingFilters
+{
+    public class TenantConsumeFilter<T> : IFilter<ConsumeContext<T>>
+            where T : class
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public TenantConsumeFilter(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void Probe(ProbeContext context)
+        {
+            context.CreateFilterScope("tenant-consume-filter");
+        }
+
+        public Task Send(ConsumeContext<T> context, IPipe<ConsumeContext<T>> next)
+        {
+            var tenantIdString = context.Headers.Get<string>("tenant_id", string.Empty);
+            if (!string.IsNullOrEmpty(tenantIdString))
+            {
+                var tenantId = Guid.Parse(tenantIdString);
+                var tenantIdProvider = _serviceProvider.GetService<ITenantIdProvider>();
+                tenantIdProvider.TenantId = tenantId;
+            }
+
+            return next.Send(context);
+        }
+    }
+}
---
+++ vtb.Auth.Tenant/MessagingFilters/TenantMessageFilter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using GreenPipes;
+using MassTransit;
+
+namespace vtb.Auth.Tenant.MessagingFilters
+{
+    public class TenantMessageFilter<T>
+        : IFilter<SendContext<T>>, IFilter<PublishContext<T>>
+        where T : class
+    {
+        private readonly ITenantIdProvider _tenantIdProvider;
+
+        public TenantMessageFilter(ITenantIdProvider tenantIdProvider)
+        {
+            _tenantIdProvider = tenantIdProvider;
+        }
+
+        public void Probe(ProbeContext context)
+        {
+            context.CreateFilterScope("tenant-set-header-filter");
+        }
+
+        public Task Send(SendContext<T> context, IPipe<SendContext<T>> next)
+        {
+            if (_tenantIdProvider.TenantId != Guid.Empty)
+            {
+                context.Headers.Set("tenant_id", _tenantIdProvider.TenantId.ToString());
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task Send(PublishContext<T> context, IPipe<PublishContext<T>> next)
+        {
+            if (_tenantIdProvider.TenantId != Guid.Empty)
+            {
+                context.Headers.Set("tenant_id", _tenantIdProvider.TenantId.ToString());
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
